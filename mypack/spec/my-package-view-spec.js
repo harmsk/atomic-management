e28515b41464f79643f23be190ac22c5856a97d2
Daniel Hirsch <djh329@cornell.edu>
2018-03-12 23:35:04 -0400

    Added my pack

---
+++ mypack/spec/my-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TylerTestPkgView from '../lib/tyler-test-pkg-view';
+
+describe('TylerTestPkgView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
