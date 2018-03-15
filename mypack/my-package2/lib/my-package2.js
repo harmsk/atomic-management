e97ffe525bd07c8b16e14a8ab1429995ef288315
jf773 <jf773@cornell.edu>
2018-03-15 13:00:42 -0400

    test flow

--- mypack/my-package2/lib/my-package2.js
+++ mypack/my-package2/lib/my-package2.js
@@ -1,7 +1,7 @@
 'use babel';
 
 // import TylerTestPkgView from './tyler-test-pkg-view';
-// import { CompositeDisposable } from 'atom';
+import { CompositeDisposable } from 'atom';
 const fs = require('fs');
 const path = require('path');
 
@@ -12,24 +12,28 @@ export default {
     let editor = pane.activeItem
     let elementToStyle = atom.views.getView(editor)
     const keys = Object.keys(config)
+    console.log(elementToStyle.style)
     keys.forEach((key) => {
-      switch(key) {
-        case "fontSize":
-            editor.
-            break;
-        default:
-          console.log(elementToStyle.style)
-          if (elementToStyle.style.hasOwnProperty(key)) {
-            elementToStyle.style[key] = config[key]
-            elementToStyle.fontSize = 20;
-            console.log(elementToStyle.style[key])
-          }
+      if (elementToStyle.style.hasOwnProperty(key)) {
+        elementToStyle.style[key] = config[key]
+        elementToStyle.style["fontSize"] = "20";
+        console.log(elementToStyle.style[key])
       }
     })
+    paneStyle = JSON.stringify(elementToStyle.style)
+    paneFilePath = editor.getPath();
+    dirName = path.dirname(paneFilePath);
+    fileName = path.join(dirName, 'test.json');
+    console.log(fileName);
+    fs.writeFile(fileName, paneStyle, (err) => {
+      if (err) throw err;
+      console.log("The file was succesfully saved!");
+    });
   },
 
   paneOps(pane) {
     // If Pane is TextEditor
+    console.log('pane')
     if (pane.activeItem &&
        pane.activeItem.__proto__.constructor.name &&
        pane.activeItem.__proto__.constructor.name == "TextEditor") {
@@ -38,7 +42,14 @@ export default {
   },
 
   activate(state) {
+    atom.notifications.addSuccess('activate now')
     console.log("activated now");
+
+    this.subscriptions = new CompositeDisposable()
+
+    this.subscriptions.add(atom.commands.add('atom-workspace', {
+      'my-package2:toggle': () => this.toggle()
+    }))
 
     atom.workspace.observeTextEditors((editor) =>
       editor.onDidSave(() => {
@@ -56,6 +67,7 @@ export default {
   },
 
   loadConfig(filePath, applyConfig, pane) {
+    console.log('loadConfig')
     dirName = path.dirname(filePath)
     fs.readdir(dirName,
       (err, files) => {
@@ -65,10 +77,15 @@ export default {
             fileName = path.join(dirName, file)
             const config = JSON.parse(fs.readFileSync(fileName))
             applyConfig(config, pane)
+
           }
         })
       }
     })
+  },
+  toggle() {
+    atom.workspace.getPanes().map((pane) => {
+         this.paneOps(pane);
+    });
   }
-
 };
--- mypack/my-package2/spec/my-package2-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import MyPackage2View from '../lib/my-package2-view';
-
-describe('MyPackage2View', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
