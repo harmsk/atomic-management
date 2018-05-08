1224c0d3b89b0a17187a8e19623ffad69f072e17
Weiyou Dai <weiyoud@dhcp-rhodes-3059.eduroam.cornell.edu>
2018-05-08 01:06:07 -0400

    added workable test suits

--- spec/config-spec.js
+++ spec/config-spec.js
@@ -3,19 +3,57 @@
 import AtomicManagement from '../lib/atomic-management';
 const fs = require('fs');
 const path = require('path');
+const temp = require('temp')
 
-describe('backgroundColor', () => {
-    // let contents;
-    // let configName = __dirname + '/.atom/config.cson';
-    // try {
-    //     contents = CSON.readFileSync(configName);
-    // } catch(e) {
-    //     console.log(e);
-    // }
-    it('fontSize failed to change', () => {
-        // for(let key in contents) {
-        //     expect(atom.config.get(key)).not.toBe(contents[key]);
-        // }
-        expect(atom.config.get("editor.fontSize")).toBe(15);
-    });
+// describe('backgroundColor', () => {
+//     // let contents;
+//     // let configName = __dirname + '/.atom/config.cson';
+//     // try {
+//     //     contents = CSON.readFileSync(configName);
+//     // } catch(e) {
+//     //     console.log(e);
+//     // }
+//
+//     it('fontSize failed to change', () => {
+//         // for(let key in contents) {
+//         //     expect(atom.config.get(key)).not.toBe(contents[key]);
+//         // }
+//
+//         expect(atom.config.get("editor.fontSize")).toBe(16);
+//     });
+// });
+
+describe('Change configurations of a project', () => {
+  let editor, buffer, filePath
+  beforeEach(() => {
+    //const directory = temp.mkdirSync()
+    const directory = '/Users/weiyoud/github/AtomicManagement'
+    atom.project.setPaths([directory])
+    filePath = path.join(directory, '/.atom/config.cson')
+
+    editor = atom.workspace.open(filePath)
+    console.log(atom.config.get());
+  })
+
+  it('toggles the package', () => {
+      atom.workspace.open(filePath).then((editor) => {
+          expect(atom.config.get('atomic-management')).not.toBeDefined()
+          AtomicManagement.toggle()
+          expect(atom.config.get('atomic-management.isEnabled')).toBe(true)
+          AtomicManagement.toggle()
+          expect(atom.config.get('atomic-management.isEnabled')).toBe(false)
+      })
+  })
+
+  it('enforces to toggle the package', () => {
+      atom.workspace.open(filePath).then((editor) => {
+          expect(atom.config.get('atomic-management')).not.toBeDefined()
+          AtomicManagement.toggleEnforced()
+          console.log(atom.config.get());
+          expect(atom.config.get('atomic-management.enforceConfiguredPackages')).toBe(true)
+          AtomicManagement.toggle()
+          console.log(atom.config.get());
+          expect(atom.config.get('atomic-management.enforceConfiguredPackages')).toBe(false)
+      })
+  })
 });
