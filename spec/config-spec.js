77ceb5461bb2dd32a7aff804458dc198c27794b4
jf773 <jf773@cornell.edu>
2018-05-08 16:20:14 -0400

    read config file

--- spec/config-spec.js
+++ spec/config-spec.js
@@ -5,17 +5,18 @@ const fs = require('fs');
 const path = require('path');
 
 describe('backgroundColor', () => {
-    // let contents;
-    // let configName = __dirname + '/.atom/config.cson';
-    // try {
-    //     contents = CSON.readFileSync(configName);
-    // } catch(e) {
-    //     console.log(e);
-    // }
-    it('should change the backgroundColor of the panel', () => {
-        // for(let key in contents) {
-        //     expect(atom.config.get(key)).not.toBe(contents[key]);
-        // }
+    // #atom.config.get("editor.fontSize") == 12
+    let contents;
+    let configName = __dirname + '/.atom/config.cson';
+    try {
+        contents = CSON.readFileSync(configName);
+    } catch(e) {
+        console.log(e);
+    }
+    it('should change the font size of the panel', () => {
+        for(let key in contents) {
+            expect(atom.config.get(key)).not.toBe(contents[key]);
+        }
         expect(atom.config.get("editor.fontSize")).toBe(15);
     });
 });
