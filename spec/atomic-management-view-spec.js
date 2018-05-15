93c359a8d249d6bd024556370babc3cfa01fcfc6
Weiyou Dai <weiyoud@dhcp-hol-4617.eduroam.cornell.edu>
2018-05-15 00:46:20 -0400

    update test suites(trial)

--- spec/atomic-management-view-spec.js
+++ spec/atomic-management-view-spec.js
@@ -11,20 +11,16 @@ describe('two ways of toggling a project', () => {
   it('toggles the package', () => {
           expect(atom.config.get('atomic-management')).not.toBeDefined()
           AtomicManagement.toggle()
-          console.log(atom.config.get());
           expect(atom.config.get('atomic-management.isEnabled')).toBe(true)
           AtomicManagement.toggle()
-          console.log(atom.config.get());
           expect(atom.config.get('atomic-management.isEnabled')).toBe(false)
   })
 
   it('enforces to toggle the package', () => {
           expect(atom.config.get('atomic-management')).not.toBeDefined()
           AtomicManagement.toggleEnforced()
-          console.log(atom.config.get());
           expect(atom.config.get('atomic-management.enforceConfiguredPackages')).toBe(true)
           AtomicManagement.toggleEnforced()
-          console.log(atom.config.get());
           expect(atom.config.get('atomic-management.enforceConfiguredPackages')).toBe(false)
   })
 });
@@ -44,7 +40,49 @@ describe('gets disabled packages', () => {
 
     it('opens configuration files', () => {
         AtomicManagement.currentConfig = filePath
-        console.log(AtomicManagement.currentConfig)
         AtomicManagement.openConfigFile()
+    })
+});
+
+describe("atomic-management default toggle", () => {
+  let activationPromise, workspaceElement
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace)
+    activationPromise = atom.packages.activatePackage('atomic-management')
+    })
+describe ("when the atomic-management:toggle() event is triggered", () => {
+    it ("attaches and then detaches the view", () => {
+        expect(workspaceElement.querySelector('.atomic-management')).not.toExist()
+
+        //  This is an activation event, triggering it will cause the package to be
+        //  activated.
+        atom.commands.dispatch(workspaceElement, 'atomic-management:toggle()')
+        runs => {
+            expect(workspaceElement.querySelector('.atomic-management')).toExist()
+            atom.commands.dispatch(workspaceElement, 'atomic-management:toggle()')
+            expect(workspaceElement.querySelector('.atomic-management')).not.toExist()
+        };
+    });
+    });
+});
+
+describe('when the atom text editor is destroyed', () => {
+    let editor, workspaceElement, filePath
+    beforeEach(async() => {
+        const directory = temp.mkdirSync()
+        atom.project.setPaths([directory])
+        workspaceElement = atom.views.getView(atom.workspace)
+        filePath = path.join(directory, 'testing.txt')
+        editor =  atom.workspace.open(filePath)
+        await atom.packages.activatePackage('atomic-management')
+        editor.destroy()
+    })
+
+    it('does not leak subscriptions', async() => {
+      const {atomicmanagement} = atom.packages.getActivePackage('atomic-management').mainModule
+      expect(atomicmanagement.subscriptions.disposables.size).toBe(5)
+
+      await atom.packages.deactivatePackage('atomic-management')
+      expect(atomicmanagement.subscriptions.disposables).toBeNull()
     })
 });
