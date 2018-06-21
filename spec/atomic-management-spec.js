54f1c1a281df619bbdc5b640acec923c16daaa8f
Kyle J. Harms <kyle.harms@cornell.edu>
2018-06-21 14:41:03 -0400

    Format code consistently
    remove commented out code.
    auto format all source files.

--- spec/atomic-management-spec.js
+++ spec/atomic-management-spec.js
@@ -6,37 +6,36 @@ const path = require('path');
 const CSON = require('season');
 
 describe("verify that the config of current window be changed as .atom/confing.cson", () => {
-    it('should be consistent with the global config', () => {
-        let globalConfig, userConfig
-        try {
-            globalConfig = CSON.readFileSync(__dirname + "/test.cson");
-            userConfig = CSON.readFileSync(__dirname + "/../.atom/config.cson");
-        } catch(e) {
-            console.log(e);
+  it('should be consistent with the global config', () => {
+    let globalConfig, userConfig
+    try {
+      globalConfig = CSON.readFileSync(__dirname + "/test.cson");
+      userConfig = CSON.readFileSync(__dirname + "/../.atom/config.cson");
+    } catch(e) {
+      console.log(e);
+    }
+    expect(globalConfig).not.toBeNull();
+    expect(userConfig).not.toBeNull();
+    function compare(user, glob) {
+      for(var toplevel in user) {
+        for(var keyPath in user[toplevel]) {
+          var vals = user[toplevel][keyPath]
+          if(Array.isArray(vals)) {
+            vals.forEach(val => {
+              console.log(val)
+              expect(glob[toplevel][keyPath]).toContain(val)
+            })
+          } else {
+            console.log(vals)
+            expect(glob[toplevel][keyPath]).toEqual(vals)
+          }
         }
-        expect(globalConfig).not.toBeNull();
-        expect(userConfig).not.toBeNull();
-        function compare(user, glob) {
-            for(var toplevel in user) {
-                for(var keyPath in user[toplevel]) {
-                    var vals = user[toplevel][keyPath]
-                    if(Array.isArray(vals)) {
-                        vals.forEach(val => {
-                            console.log(val)
-                            expect(glob[toplevel][keyPath]).toContain(val)
-                        })
-                    } else {
-                        console.log(vals)
-                        expect(glob[toplevel][keyPath]).toEqual(vals)
-                    }
-                }
-            }
-        }
-        if("*" in userConfig) {
-            compare(userConfig["*"], globalConfig)
-        } else {
-            compare(userConfig["*"], globalConfig)
-        }
-
-    });
+      }
+    }
+    if("*" in userConfig) {
+      compare(userConfig["*"], globalConfig)
+    } else {
+      compare(userConfig["*"], globalConfig)
+    }
+  });
 });
--- spec/atomic-management-view-spec.js
+++ spec/atomic-management-view-spec.js
@@ -7,100 +7,93 @@ const CSON = require('season')
 
 import AtomicManagement from '../lib/atomic-management';
 
-  it('enforces to toggle the package', () => {
-          expect(atom.config.get('atomic-management')).not.toBeDefined()
-          AtomicManagement.toggleEnforced()
-          expect(atom.config.get('atomic-management.enforceConfiguredPackages')).toBe(true)
-          AtomicManagement.toggleEnforced()
-          expect(atom.config.get('atomic-management.enforceConfiguredPackages')).toBe(false)
-  })
+it('enforces to toggle the package', () => {
+  expect(atom.config.get('atomic-management')).not.toBeDefined()
+  AtomicManagement.toggleEnforced()
+  expect(atom.config.get('atomic-management.enforceConfiguredPackages')).toBe(true)
+  AtomicManagement.toggleEnforced()
+  expect(atom.config.get('atomic-management.enforceConfiguredPackages')).toBe(false)
 });
 
 describe('gets disabled packages', () => {
-    let filePath, contents
-    beforeEach(() => {
-        filePath = __dirname + "/../.atom/config.cson"
-        contents = CSON.readFileSync(filePath);
-    })
-
-    // it('get disabled packages', () => {
-    //     expect(disabledPackages).not.toBeDefined();
-    //     var disabledPackages = AtomicManagement.getDisabledPackages(contents);
-    //     expect(disabledPackages[0]).toBe('php-server');
-    // })
+  let filePath, contents
+  beforeEach(() => {
+    filePath = __dirname + "/../.atom/config.cson"
+    contents = CSON.readFileSync(filePath);
+  })
 });
 
 describe('prompts users to handle packages errors', () => {
-    let packageNames, packagesMap
-    beforeEach(() => {
-        packageNames = ["haha-testing", "happy"]
-        packagesMap = new Map()
-        packagesMap.set("[]", [])
-    })
+  let packageNames, packagesMap
+  beforeEach(() => {
+    packageNames = ["haha-testing", "happy"]
+    packagesMap = new Map()
+    packagesMap.set("[]", [])
+  })
 
-    it('asks users to install packages', () => {
-        AtomicManagement.askInstallPackages(packageNames)
-    })
+  it('asks users to install packages', () => {
+    AtomicManagement.askInstallPackages(packageNames)
+  })
 
-    it('alerts users to check the configuration file', () => {
-        AtomicManagement.alertBadPackages(packageNames)
-    })
+  it('alerts users to check the configuration file', () => {
+    AtomicManagement.alertBadPackages(packageNames)
+  })
 
-    it('installs specified packages', () => {
-        AtomicManagement.installPackages(packageNames)
-    })
+  it('installs specified packages', () => {
+    AtomicManagement.installPackages(packageNames)
+  })
 
-    it('checks existance of packages', () => {
-        AtomicManagement.checkPackages(packagesMap)
-    })
+  it('checks existance of packages', () => {
+    AtomicManagement.checkPackages(packagesMap)
+  })
 
 });
 
 describe('tests status bar', () => {
-    let contentsWithAsterisk, contentsWithoutAsterisk
-    beforeEach(() => {
-        contentsWithAsterisk = {"*": "Testing"}
-        contentsWithoutAsterisk = {"": "Testing"}
-    })
+  let contentsWithAsterisk, contentsWithoutAsterisk
+  beforeEach(() => {
+    contentsWithAsterisk = {"*": "Testing"}
+    contentsWithoutAsterisk = {"": "Testing"}
+  })
 
-    it('parses a configuration file without Asterisk', () => {
-        var returnedContents = AtomicManagement.standardizeConfig(contentsWithoutAsterisk)
-        expect(JSON.stringify(returnedContents)).toBe(JSON.stringify({"*":contentsWithoutAsterisk}))
-    })
+  it('parses a configuration file without Asterisk', () => {
+    var returnedContents = AtomicManagement.standardizeConfig(contentsWithoutAsterisk)
+    expect(JSON.stringify(returnedContents)).toBe(JSON.stringify({"*":contentsWithoutAsterisk}))
+  })
 
-    it('parses a configuration file with Asterisk', () => {
-        var returnedContents = AtomicManagement.standardizeConfig(contentsWithAsterisk)
-        expect(JSON.stringify(returnedContents)).toBe(JSON.stringify(contentsWithAsterisk))
-    })
+  it('parses a configuration file with Asterisk', () => {
+    var returnedContents = AtomicManagement.standardizeConfig(contentsWithAsterisk)
+    expect(JSON.stringify(returnedContents)).toBe(JSON.stringify(contentsWithAsterisk))
+  })
 });
 
 describe('Checks "disabledPackages"', () => {
-    let contentsWithAsterisk, contentsWithoutAsterisk, contentsWithAsteriskAndDisabledPackages
-    beforeEach(() => {
-        contentsWithAsterisk = {"*": "Testing"}
-        contentsWithoutAsterisk = {"": "Testing"}
-        contentsWithAsteriskAndDisabledPackages = {"*": {"core":{"disabledPackages":["testing", "package"]}}}
-    })
+  let contentsWithAsterisk, contentsWithoutAsterisk, contentsWithAsteriskAndDisabledPackages
+  beforeEach(() => {
+    contentsWithAsterisk = {"*": "Testing"}
+    contentsWithoutAsterisk = {"": "Testing"}
+    contentsWithAsteriskAndDisabledPackages = {"*": {"core":{"disabledPackages":["testing", "package"]}}}
+  })
 
-    it('checks if there are two packages to be disabled', () => {
-        var returnedContents = AtomicManagement.getDisabledPackages(contentsWithAsteriskAndDisabledPackages)
-        expect(JSON.stringify(returnedContents)).toBe(JSON.stringify(["testing", "package"]))
-    })
+  it('checks if there are two packages to be disabled', () => {
+    var returnedContents = AtomicManagement.getDisabledPackages(contentsWithAsteriskAndDisabledPackages)
+    expect(JSON.stringify(returnedContents)).toBe(JSON.stringify(["testing", "package"]))
+  })
 
-    it('checks if there is no disabledPackage or core', () => {
-        var returnedContents = AtomicManagement.getDisabledPackages(contentsWithoutAsterisk)
-        expect(JSON.stringify(returnedContents)).toBe(JSON.stringify([]))
-    })
+  it('checks if there is no disabledPackage or core', () => {
+    var returnedContents = AtomicManagement.getDisabledPackages(contentsWithoutAsterisk)
+    expect(JSON.stringify(returnedContents)).toBe(JSON.stringify([]))
+  })
 });
 
 describe('reloads', () => {
-    it('asks users to reload packages', () => {
-        AtomicManagement.askReload("testing", "")
-    })
+  it('asks users to reload packages', () => {
+    AtomicManagement.askReload("testing", "")
+  })
 });
 
 describe('global configuration', () => {
-    it('restores global configurations', () => {
-        AtomicManagement.restoreGlobalConfig()
-    })
+  it('restores global configurations', () => {
+    AtomicManagement.restoreGlobalConfig()
+  })
 });
